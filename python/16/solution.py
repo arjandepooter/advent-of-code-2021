684823df845260ce6a0e766855210bd2ec52f6aa
Arjan de Pooter <mail@arjandepooter.nl>
2021-12-16 08:58:40 +0100

    Day 16 cleanup

--- python/16/solution.py
+++ python/16/solution.py
@@ -1,6 +1,6 @@
 import sys
 from functools import reduce
-from typing import List, Tuple
+from typing import Callable, Dict, List, Tuple
 
 
 class Package:
@@ -28,6 +28,17 @@ class Literal(Package):
         return self.value
 
 
+operators: Dict[int, Callable[[List[int]], int]] = {
+    0: sum,
+    1: lambda children: reduce(lambda a, b: a * b, children, 1),
+    2: min,
+    3: max,
+    5: lambda children: int(children[0] > children[1]),
+    6: lambda children: int(children[0] < children[1]),
+    7: lambda children: int(children[0] == children[1]),
+}
+
+
 class Operator(Package):
     def __init__(self, version: int, type_: int, children: List[Package]):
         self.version = version
@@ -38,40 +49,25 @@ class Operator(Package):
         return f"Operator(v{self.version}, {self.type_}, [{', '.join(str(child) for child in self.children)}])"
 
     def get_version(self) -> int:
-        return self.version + sum(
-            [child.get_version() for child in self.children]
-        )
+        return self.version + sum([child.get_version() for child in self.children])
 
     def evaluate(self) -> int:
         children = [child.evaluate() for child in self.children]
 
-        match self.type_:            
-            case 0:
-                return sum(children)
-            case 1:
-                return reduce(lambda a, b: a * b, children, 1)
-            case 2:
-                return min(children)
-            case 3:
-                return max(children)
-            case 5:
-                return int(children[0] > children[1])
-            case 6:
-                return int(children[0] < children[1])
-            case 7:
-                return int(children[0] == children[1])
-            case _:
-                return 0
+        return operators.get(self.type_, lambda _: 0)(children)
+
 
 def read_input() -> List[bool]:
     line = [line.strip() for line in sys.stdin.readlines() if line.strip()][0]
     result: List[bool] = []
     for n in line:
-        result += [n == '1' for n in bin(int(n, 16))[2:].zfill(4)]
+        result += [n == "1" for n in bin(int(n, 16))[2:].zfill(4)]
     return result
 
+
 def to_int(bits: List[bool]) -> int:
-    return reduce(lambda acc, b: (acc << 1) | b,  bits, 0)
+    return reduce(lambda acc, b: (acc << 1) | b, bits, 0)
+
 
 def read_varint(data: List[bool]) -> Tuple[int, List[bool]]:
     acc = 0
@@ -84,7 +80,7 @@ def read_varint(data: List[bool]) -> Tuple[int, List[bool]]:
         if not cont:
             break
 
-    return acc, data[offset: ]
+    return acc, data[offset:]
 
 
 def read_int(data: List[bool], length: int) -> Tuple[int, List[bool]]:
@@ -94,7 +90,7 @@ def read_int(data: List[bool], length: int) -> Tuple[int, List[bool]]:
 def decode(data: List[bool]) -> Tuple[Package, List[bool]]:
     version, data = read_int(data, 3)
     type_, data = read_int(data, 3)
-    
+
     if type_ == 4:
         value, data = read_varint(data)
         return Literal(version, value), data
@@ -105,7 +101,7 @@ def decode(data: List[bool]) -> Tuple[Package, List[bool]]:
     if type_id == 0:
         sub_length, data = read_int(data, 15)
         sub_data, data = data[:sub_length], data[sub_length:]
-        
+
         while len(sub_data):
             child, sub_data = decode(sub_data)
             children.append(child)
@@ -116,6 +112,7 @@ def decode(data: List[bool]) -> Tuple[Package, List[bool]]:
             children.append(child)
 
     return Operator(version, type_, children), data
+
 
 def part_1(data: List[bool]) -> int:
     package, _ = decode(data)
